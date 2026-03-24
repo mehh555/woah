75df2def6890aebb8fd59eb2cfeac7638b0a3477
mehh555 <essayikes@gmail.com>
2026-03-24 22:17:19 +0100

    fix port fallack

--- backend/src/Woah.Api/Program.cs
+++ backend/src/Woah.Api/Program.cs
@@ -42,7 +42,8 @@ if (connectionString?.StartsWith("postgres://") == true || connectionString?.Sta
 {
     var uri = new Uri(connectionString);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 builder.Services.AddDbContext<WoahDbContext>(options =>
